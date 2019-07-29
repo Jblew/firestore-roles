f117a31985f0798c57446a4e9fd192fa3983cb1a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-29 21:52:28 +0200

    feat: implement removeFromRequestedRoles()

--- src/FirestoreRoles.ts
+++ src/FirestoreRoles.ts
@@ -51,14 +51,43 @@ export class FirestoreRoles {
 
         await this.saveRoles(uid, roles);
     }
+
+    public async removeFromRequestedRoles(uid: string, rolesToRemove: string[]) {
+        ow(uid, "FirestoreRoles.removeFromRequestedRoles(uid)", ow.string.nonEmpty);
+        ow(
+            rolesToRemove,
+            "FirestoreRoles.removeFromRequestedRoles(rolesToRemove)",
+            ow.array.ofType(ow.string.nonEmpty.is(v => FirestoreRolesConfiguration.isAllowedRole(this.config, v))),
+        );
+
+        await this.firestore.runTransaction(async () => await this.doRemoveFromRequestedRoles(uid, rolesToRemove));
+    }
+
     private async doRequestRoles(uid: string, newRolesToRequest: string[]) {
         const accountRecord = await this.getAccountRecord(uid);
         const updatedRequestedRoles = _.uniq([...accountRecord.requestedRoles, ...newRolesToRequest]);
         await this.saveRequestedRoles(uid, updatedRequestedRoles);
     }
-        AccountRecord.validate(accountRecord, this.config);
 
-        await this.getUserDoc(user.uid).set(accountRecord);
+    private async doRemoveFromRequestedRoles(uid: string, rolesToRemove: string[]) {
+        const accountRecord = await this.getAccountRecord(uid);
+        const updatedRequestedRoles = _.without(accountRecord.requestedRoles, ...rolesToRemove);
+        await this.saveRequestedRoles(uid, updatedRequestedRoles);
+    }
+
+    private async getAccountRecord(uid: string): Promise<AccountRecord> {
+        const arSnapshot = await this.getUserDoc(uid).get();
+        if (!arSnapshot.exists) throw new FirestoreRolesError("User does not exist");
+        const ar = arSnapshot.data() as AccountRecord;
+        AccountRecord.validate(ar, this.config);
+        return ar;
+    }
+
+    private async saveAccountRecord(accountRecord: AccountRecord) {
+        AccountRecord.validate(accountRecord, this.config);
+        await this.getUserDoc(accountRecord.uid).set(accountRecord);
+    }
+
     private async saveRequestedRoles(uid: string, requestedRoles: string[]) {
         const fieldsToUpdate: RequestedRolesHolder = {
             requestedRoles,

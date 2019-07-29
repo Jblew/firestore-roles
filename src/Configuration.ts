38d9f8a8c5cbf2a1cd10d4a369e23423ae084a0b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-29 23:50:03 +0200

    feat: validate managed roles in Configuration

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -1,5 +1,5 @@
 // tslint:disable no-shadowed-variable
-
+import * as _ from "lodash";
 import ow from "ow";
 
 import { ow_catch } from "./util";
@@ -13,10 +13,13 @@ export namespace Configuration {
     export function validate(c: Configuration) {
         ow(c.accountsCollection, "Configuration.accountsCollection", ow.string.nonEmpty);
 
+        const roleNames = _.keys(c);
         ow(
             c.roles,
             "Configuration.roles",
-            ow.object.valuesOfType(ow.object.is(o => ow_catch(() => Configuration.Role.validate(o as Role)))),
+            ow.object.valuesOfType(
+                ow.object.is(o => ow_catch(() => Configuration.Role.validate(o as Role, roleNames))),
+            ),
         );
     }
 
@@ -29,8 +32,8 @@ export namespace Configuration {
     }
 
     export namespace Role {
-        export function validate(r: Role) {
-            ow(r.manages, "Role.manages", ow.array.ofType(ow.string.nonEmpty));
+        export function validate(r: Role, roleNames: string[]) {
+            ow(r.manages, "Role.manages", ow.array.ofType(ow.string.nonEmpty.oneOf(roleNames)));
         }
     }
 }

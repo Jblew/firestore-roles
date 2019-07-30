a68f522092a99d0b47d0a12a2a7f4d9a282c695b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-30 21:45:35 +0200

    test: define generated rules tests

--- src/RulesGenerator.integration.test.ts
+++ src/RulesGenerator.integration.test.ts
@@ -27,7 +27,7 @@ describe.only("RulesGenerator", function() {
     describe("Generated rules for accounts collection", () => {
         const col = config.accountsCollection;
         describe("Not authenticated user", () => {
-            it.only("Cannot create in accounts collection", async () => {
+            it("Cannot create in accounts collection", async () => {
                 const { userFirestore } = await mock({ uid: undefined, config, customRules: "" });
 
                 await assert.isRejected(userFirestore.collection(col).add({ da: "ta" }), /PERMISSION_DENIED/);
@@ -64,5 +64,45 @@ describe.only("RulesGenerator", function() {
             it.skip("Cannot list from accounts collection");
         });
 
+        describe("Authenticated, not manager", () => {
+            it.skip("Can create account with own uid");
+            it.skip("Can create account with own uid with empty roles field");
+            it.skip("Cannot create account with own uid with not empty roles field");
+            it.skip("Cannot create account with not own uid");
+            it.skip("Can read account with own uid");
+            it.skip("Cannot read account with not own uid");
+            it.skip("Cannot list");
+            it.skip("Can request a role");
+            it.skip("Can remove role from requested roles");
+            it.skip("Cannot set roles field");
+            it.skip("Cannot delete a role");
+            it.skip("Cannot update roles field");
+            it.skip("Cannot update other fields");
+        });
+
+        describe("Authenticated, manager", () => {
+            it.skip("Cannot update or set own roles field");
+            it.skip("Cannot create account with own uid with not empty roles field");
+            it.skip("Cannot create account with not own uid");
+            it.skip("Can list users with roles he manages");
+            it.skip("Cannot list users with roles that he doesnt manage");
+            it.skip("Can get users with roles he manages");
+            it.skip("Cannot get users with roles that he doesnt manage");
+            it.skip("Can add role that he manages");
+            it.skip("Cannot add role that he doesnt manage");
+            it.skip("Can remove role that he manages");
+            it.skip("Cannot remove role that he doesnt manage");
+            it.skip("Can add requestedRoles that he manages");
+            it.skip("Cannot add requestedRoles that he doesnt manage");
+            it.skip("Can remove requestedRoles that he manages");
+            it.skip("Cannot remove requestedRoles that he doesnt manage");
+            it.skip("Cannot update or set other fields in users with roles that he manages");
+            it.skip("Cannot update or set other fields in users with roles that he doesnt manage");
+        });
+    });
+
+    describe("Guarded rules", () => {
+        it.skip("User without a required role cannot write to hasRoles() guarded collection");
+        it.skip("User with a required role can write to hasRoles() guarded collection");
     });
 });

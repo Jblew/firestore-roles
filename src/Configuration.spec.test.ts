535f3542c8c1e1f40d007a0be62a712bf9641e1a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-30 12:22:41 +0200

    test: test roles management checking

--- src/Configuration.spec.test.ts
+++ src/Configuration.spec.test.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:max-classes-per-file */
-import { use as chaiUse } from "chai";
+import { expect, use as chaiUse } from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as _ from "lodash";
 import "mocha";
@@ -9,7 +9,19 @@ import { Configuration } from "./Configuration";
 chaiUse(chaiAsPromised);
 
 describe("Configuration", () => {
-    it("Default configuration passes validation", async () => {
+    it("Default configuration passes validation", () => {
         Configuration.validate(Configuration.DEFAULT);
+    });
+
+    it("Disallows managing non-defined role", () => {
+        const config: Configuration = {
+            ...Configuration.DEFAULT,
+            roles: {
+                admin: { manages: ["nonexistentrole"] },
+                editor: { manages: [] },
+            },
+        };
+
+        expect(() => Configuration.validate(config)).to.throw("ArgumentError");
     });
 });

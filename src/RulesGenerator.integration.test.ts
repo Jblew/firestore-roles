53b1ef9c1eb852d80893fc8515a48b1574d71543
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-01 14:11:29 +0200

    test: test rules for new role requests model

--- src/RulesGenerator.integration.test.ts
+++ src/RulesGenerator.integration.test.ts
@@ -25,6 +25,7 @@ describe("RulesGenerator", function() {
     const config: Configuration = {
         accountsCollection: "accounts",
         roleCollectionPrefix: "role_",
+        roleRequestsCollectionPrefix: "role_requests_",
         roles: {
             admin: { manages: ["manager", "editor", "reviewer"] },
             manager: { manages: ["editor", "reviewer"] },
@@ -81,6 +82,42 @@ describe("RulesGenerator", function() {
 
             describe("Roles collection", () => {
                 const col = config.roleCollectionPrefix + _.keys(config.roles)[0];
+
+                it("Cannot list", async () => {
+                    const { userFirestore } = await mock({ uid: undefined, config });
+                    await assert.isRejected(userFirestore.collection(col).get(), /false for 'list'/);
+                });
+
+                it("Cannot read", async () => {
+                    const { userDoc, createAccount } = await mock({ uid: undefined, config });
+                    const createdAccount = await createAccount(uuid());
+
+                    await assert.isRejected(userDoc(col, createdAccount.uid).get(), /false for 'get'/);
+                });
+
+                it("Cannot create", async () => {
+                    const { userFirestore } = await mock({ uid: undefined, config });
+
+                    await assert.isRejected(userFirestore.collection(col).add({ uid: "uid" }), /PERMISSION_DENIED/);
+                });
+
+                it("Cannot update", async () => {
+                    const { userDoc, createAccount } = await mock({ uid: undefined, config });
+                    const createdAccount = await createAccount(uuid());
+
+                    await assert.isRejected(userDoc(col, createdAccount.uid).update({ a: "b" }), /PERMISSION_DENIED/);
+                });
+
+                it("Cannot delete", async () => {
+                    const { userDoc, createAccount } = await mock({ uid: undefined, config });
+                    const createdAccount = await createAccount(uuid());
+
+                    await assert.isRejected(userDoc(col, createdAccount.uid).delete(), /PERMISSION_DENIED/);
+                });
+            });
+
+            describe("Role requests collection", () => {
+                const col = config.roleRequestsCollectionPrefix + _.keys(config.roles)[0];
 
                 it("Cannot list", async () => {
                     const { userFirestore } = await mock({ uid: undefined, config });
@@ -198,52 +235,74 @@ describe("RulesGenerator", function() {
                 });
             });
 
-            describe("Roles collection", () => {
-                const role = _.keys(config.roles)[0];
-                const col = config.roleCollectionPrefix + role;
+            [
+                { name: "Roles collection", colPrefix: config.roleCollectionPrefix, canCreateOwnUid: false },
+                {
+                    name: "Role requests collection",
+                    colPrefix: config.roleRequestsCollectionPrefix,
+                    canCreateOwnUid: true,
+                },
+            ].forEach(colType =>
+                describe(colType.name, () => {
+                    const role = _.keys(config.roles)[0];
+                    const col = colType.colPrefix + role;
 
-                it("Can get own uid", async () => {
-                    const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
-                    await adminEnableRole(uid, role);
+                    it("Can get own uid", async () => {
+                        const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
+                        await adminEnableRole(uid, role);
 
-                    await assert.isFulfilled(userDoc(col, uid).get());
-                });
+                        await assert.isFulfilled(userDoc(col, uid).get());
+                    });
 
-                it("Can get other user uid", async () => {
-                    const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
-                    const someoneElsesUid = `someone-${uid}`;
-                    await adminEnableRole(someoneElsesUid, role);
+                    it("Can get other user uid", async () => {
+                        const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
+                        const someoneElsesUid = `someone-${uid}`;
+                        await adminEnableRole(someoneElsesUid, role);
 
-                    await assert.isFulfilled(userDoc(col, someoneElsesUid).get());
-                });
+                        await assert.isFulfilled(userDoc(col, someoneElsesUid).get());
+                    });
 
-                it("Cannot list", async () => {
-                    const { userFirestore, uid, adminEnableRole } = await mock({ uid: uuid(), config });
-                    await adminEnableRole(uid, role);
+                    it("Cannot list", async () => {
+                        const { userFirestore, uid, adminEnableRole } = await mock({ uid: uuid(), config });
+                        await adminEnableRole(uid, role);
 
-                    await assert.isRejected(userFirestore.collection(col).get(), /false for 'list'/);
-                });
+                        await assert.isRejected(userFirestore.collection(col).get(), /false for 'list'/);
+                    });
 
-                it("Cannot update", async () => {
-                    const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
-                    await adminEnableRole(uid, role);
+                    it("Cannot update", async () => {
+                        const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
+                        await adminEnableRole(uid, role);
 
-                    await assert.isRejected(userDoc(col, uid).update({ da: "ta" }), /PERMISSION_DENIED/);
-                });
+                        await assert.isRejected(userDoc(col, uid).update({ da: "ta" }), /PERMISSION_DENIED/);
+                    });
 
-                it("Cannot create", async () => {
-                    const { userDoc, uid } = await mock({ uid: uuid(), config });
+                    it((colType.canCreateOwnUid ? "Can" : "Cannot") + " create record with own uid", async () => {
+                        const { userDoc, uid } = await mock({ uid: uuid(), config });
 
-                    await assert.isRejected(userDoc(col, uid).set(FirestoreRecordKeeper), /PERMISSION_DENIED/);
-                });
+                        if (colType.canCreateOwnUid) {
+                            await assert.isFulfilled(userDoc(col, uid).set(FirestoreRecordKeeper));
+                        } else {
+                            await assert.isRejected(userDoc(col, uid).set(FirestoreRecordKeeper), /PERMISSION_DENIED/);
+                        }
+                    });
 
-                it("Cannot delete", async () => {
-                    const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
-                    await adminEnableRole(uid, role);
+                    it("Cannot create record with not own uid", async () => {
+                        const { userDoc, uid } = await mock({ uid: uuid(), config });
 
-                    await assert.isRejected(userDoc(col, uid).delete(), /PERMISSION_DENIED/);
-                });
-            });
+                        await assert.isRejected(
+                            userDoc(col, "-some-other-uid").set(FirestoreRecordKeeper),
+                            /PERMISSION_DENIED/,
+                        );
+                    });
+
+                    it("Cannot delete", async () => {
+                        const { userDoc, uid, adminEnableRole } = await mock({ uid: uuid(), config });
+                        await adminEnableRole(uid, role);
+
+                        await assert.isRejected(userDoc(col, uid).delete(), /PERMISSION_DENIED/);
+                    });
+                }),
+            );
         });
 
         describe("Authenticated, manager", () => {
@@ -333,77 +392,101 @@ describe("RulesGenerator", function() {
                 });
             });
 
-            describe("Roles collection", () => {
-                it("Can list in role he manages", async () => {
-                    const role = managedUser.role;
-                    const { userFirestore } = await mock({ uid: manager.account.uid, config, projectId });
+            [
+                {
+                    name: "Roles collection",
+                    colPrefix: config.roleCollectionPrefix,
+                    canCreateNotOwnUidInRoleHeManages: true,
+                },
+                {
+                    name: "Role requests collection",
+                    colPrefix: config.roleRequestsCollectionPrefix,
+                    canCreateNotOwnUidInRoleHeManages: false,
+                },
+            ].forEach(colType =>
+                describe(colType.name, () => {
+                    it("Can list in role he manages", async () => {
+                        const role = managedUser.role;
+                        const { userFirestore } = await mock({ uid: manager.account.uid, config, projectId });
 
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isFulfilled(userFirestore.collection(col).get());
-                });
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isFulfilled(userFirestore.collection(col).get());
+                    });
 
-                it("Cannot list in role that he doesnt manage", async () => {
-                    const role = notManagedUser.role;
-                    const { userFirestore } = await mock({ uid: manager.account.uid, config, projectId });
+                    it("Cannot list in role that he doesnt manage", async () => {
+                        const role = notManagedUser.role;
+                        const { userFirestore } = await mock({ uid: manager.account.uid, config, projectId });
 
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isRejected(userFirestore.collection(col).get(), /false for 'list'/);
-                });
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isRejected(userFirestore.collection(col).get(), /false for 'list'/);
+                    });
 
-                it("Can create in role he manages", async () => {
-                    const role = managedUser.role;
-                    const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
+                    it(
+                        (colType.canCreateNotOwnUidInRoleHeManages ? "Can" : "Cannot") +
+                            " create not own uid in role he manages",
+                        async () => {
+                            const role = managedUser.role;
+                            const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
 
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isFulfilled(userDoc(col, "doc").set(FirestoreRecordKeeper));
-                });
-
-                it("Cannot create in role that he doesnt manage", async () => {
-                    const role = notManagedUser.role;
-                    const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
-
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isRejected(userDoc(col, "doc").set(FirestoreRecordKeeper), /PERMISSION_DENIED/);
-                });
-
-                it("Cannot update in list that he manages", async () => {
-                    const role = managedUser.role;
-                    const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
-
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isRejected(
-                        userDoc(col, managedUser.account.uid).update({ da: "ta" }),
-                        /PERMISSION_DENIED/,
+                            const col = `${colType.colPrefix}${role}`;
+                            if (colType.canCreateNotOwnUidInRoleHeManages) {
+                                await assert.isFulfilled(userDoc(col, "-not-own-uid-").set(FirestoreRecordKeeper));
+                            } else {
+                                await assert.isRejected(
+                                    userDoc(col, "-not-own-uid-").set(FirestoreRecordKeeper),
+                                    /PERMISSION_DENIED/,
+                                );
+                            }
+                        },
                     );
-                });
 
-                it("Cannot update in list that he doesn't manage", async () => {
-                    const role = notManagedUser.role;
-                    const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
+                    it("Cannot create in role that he doesnt manage", async () => {
+                        const role = notManagedUser.role;
+                        const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
 
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isRejected(
-                        userDoc(col, notManagedUser.account.uid).update({ da: "ta" }),
-                        /PERMISSION_DENIED/,
-                    );
-                });
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isRejected(userDoc(col, "doc").set(FirestoreRecordKeeper), /PERMISSION_DENIED/);
+                    });
 
-                it("Can delete in list that he manages", async () => {
-                    const role = managedUser.role;
-                    const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
+                    it("Cannot update in list that he manages", async () => {
+                        const role = managedUser.role;
+                        const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
 
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isFulfilled(userDoc(col, managedUser.account.uid).delete());
-                });
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isRejected(
+                            userDoc(col, managedUser.account.uid).update({ da: "ta" }),
+                            /PERMISSION_DENIED/,
+                        );
+                    });
 
-                it("Cannot delete in list that he doesn't manage", async () => {
-                    const role = notManagedUser.role;
-                    const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
+                    it("Cannot update in list that he doesn't manage", async () => {
+                        const role = notManagedUser.role;
+                        const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
 
-                    const col = `${config.roleCollectionPrefix}${role}`;
-                    await assert.isRejected(userDoc(col, notManagedUser.account.uid).delete(), /PERMISSION_DENIED/);
-                });
-            });
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isRejected(
+                            userDoc(col, notManagedUser.account.uid).update({ da: "ta" }),
+                            /PERMISSION_DENIED/,
+                        );
+                    });
+
+                    it("Can delete in list that he manages", async () => {
+                        const role = managedUser.role;
+                        const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
+
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isFulfilled(userDoc(col, managedUser.account.uid).delete());
+                    });
+
+                    it("Cannot delete in list that he doesn't manage", async () => {
+                        const role = notManagedUser.role;
+                        const { userDoc } = await mock({ uid: manager.account.uid, config, projectId });
+
+                        const col = `${colType.colPrefix}${role}`;
+                        await assert.isRejected(userDoc(col, notManagedUser.account.uid).delete(), /PERMISSION_DENIED/);
+                    });
+                }),
+            );
         });
     });
 
